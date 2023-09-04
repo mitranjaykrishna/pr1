a22cc3662d70c94832ed27752f015e2890df6746
Mitranjay Krishna <mitranjaykrishna@gmail.com>
2023-09-04 21:23:48 +0530

    usestate and design improve

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,21 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import FileCopyOutlinedIcon from "@mui/icons-material/FileCopyOutlined";
 import { Button, Checkbox, Slider } from "@mui/material";
+
 const Card = () => {
+  const [sliderValue, setSliderValue] = useState(50);
+
+  const handleSliderChange = (event, newValue) => {
+    setSliderValue(newValue);
+  };
+
   return (
-    <div className="w-[30%]  bg-slate-400 flex flex-col gap-5 p-5 rounded-lg ">
-      <div className="flex justify-between">
+    <div className="w-[30%]  bg-[#352F44] flex flex-col gap-5 p-5 rounded-lg text-[#B9B4C7] font-semibold ">
+      <div className="flex justify-between text-[#fff]">
         <h1>PS3&SVBIA</h1>
-        <FileCopyOutlinedIcon />
+        <div className=" cursor-pointer">
+          {" "}
+          <FileCopyOutlinedIcon />
+        </div>
       </div>
       <div className="flex flex-col gap-2">
         <div className="flex justify-between">
           <h1>Character Length</h1>
-          <h1>4</h1>
+          <h1>{sliderValue}</h1>
         </div>
         <div>
           <Slider
-            defaultValue={50}
+            value={sliderValue}
+            onChange={handleSliderChange}
             aria-label="Default"
             valueLabelDisplay="auto"
           />
@@ -69,11 +80,16 @@ const Card = () => {
         </div>
       </div>
 
-      <div className="flex justify-between">
+      <div className="flex justify-between bg-[#272829] p-5 rounded-lg ">
         <h1>Strength</h1>
         <p>Poor</p>
       </div>
-      <Button variant="contained">Generate</Button>
+      <Button
+        variant="contained"
+        sx={{ backgroundColor: "#176B87", height: "3rem" }}
+      >
+        Generate
+      </Button>
     </div>
   );
 };
